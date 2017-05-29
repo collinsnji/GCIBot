349611166af4d9a3b7855be3347264a36693df1f
collinsnji <collins.geek@gmail.com>
2017-05-29 20:33:42 +0100

    Some simple messages added for more interactivity

--- index.js
+++ index.js
@@ -2,22 +2,30 @@ const TelegramBotAPI = require('node-telegram-bot-api');
 const fs = require('fs');
 
 var config = JSON.parse(fs.readFileSync("config.json")),
-    API_KEY = config.API_KEY;
+	API_KEY = config.API_KEY;
 
 var telegram = new TelegramBotAPI(API_KEY, { polling: true });
-var TripA = "Deniz Karakay \nAugust van de Ven \nScott Moses \nIsaac Ong \nOana Roșca \nKaisar Arkhan (Yuki) \nDaniel Hsing \nAnshuman Agarwal \nCristian Garcia \nTymon Radzik \nDhruv Shrivastava Sawan Kumar \nJacqueline Bronger \nSoham Sen \nFilip Grzywok \nJustin Du";
-var TripB = "Evgeny Shulgin \nAlexandru Bratosin\n Sergey Popov \nIlya Bizyaev \nSampriti Panda \nTommy Ip \n Utkarsh Dixit \nDhanat Satta-awalo \nCollin Grimm \nMira Yang \nJoshua Pan \nShriank Kanaparti \nShardul Chiplunkar \nMatthew Marting \nRaefaldhi Amartya Junior \nVanisha Kesswani \nMichal Hanus \nSudhanshu Agarwal \n"
+
+var TripA = "TRIP A\n ======\nDeniz Karakay \nAugust van de Ven \nScott Moses \nIsaac Ong \nOana Roșca \nKaisar Arkhan (Yuki) \nDaniel Hsing \nAnshuman Agarwal \nCristian Garcia \nTymon Radzik \nDhruv Shrivastava Sawan Kumar \nJacqueline Bronger \nSoham Sen \nFilip Grzywok \nJustin Du";
+var TripB = "TRIP B\n ======\nEvgeny Shulgin \nAlexandru Bratosin\n Sergey Popov \nIlya Bizyaev \nSampriti Panda \nTommy Ip \n Utkarsh Dixit \nDhanat Satta-awalo \nCollin Grimm \nMira Yang \nJoshua Pan \nShriank Kanaparti \nShardul Chiplunkar \nMatthew Marting \nRaefaldhi Amartya Junior \nVanisha Kesswani \nMichal Hanus \nSudhanshu Agarwal \n"
+
+var words = [
+	"Trip A", "Trip B", "K", "lol", "Anime", "...", "dude",
+	"@CollinGrimm", "fuck", "stupid", "it works", "who are you"];
+var response = [
+	TripA, TripB, "Kek", "No LOLs", "Is that a thing?", "Dude, enough with the ... thingy xD",
+	"Hey dudie", "Grimm is my god", "The F word is offensive", "You are stupid", "Damn right it works!",
+	"I'm your boss!"];
 
 telegram.on("text", (message) => {
-    var messageVal = message.text.toLowerCase();
-    switch (messageVal) {
-        case "/tripa":
-            telegram.sendMessage(message.chat.id, TripA);
-            break;
-        case "/tripb":
-            telegram.sendMessage(message.chat.id, TripB);
-            break;
-        default:
-            break;
-    }
+	for (var i = 0; i < words.length; i++) {
+		var res = response[i];
+		if (message.text.toLowerCase().includes(words[i].toLowerCase())) {
+			console.log(message.chat.id);
+			return telegram.sendMessage(message.chat.id, res);
+		}
+		if (message.text.toLowerCase() == words[i].toLowerCase()) {
+			return telegram.sendMessage(message.chat.id, res);
+		}
+	}
 });
