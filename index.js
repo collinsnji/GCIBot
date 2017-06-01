4326f5a6245fa64f2784905c0eadecb74db54cf2
collinsnji <collins.geek@gmail.com>
2017-06-01 12:52:45 +0100

    Keeping it simple

--- index.js
+++ index.js
@@ -7,22 +7,17 @@ var config = JSON.parse(fs.readFileSync("config.json")),
 var telegram = new TelegramBotAPI(API_KEY, { polling: true });
 
 var TripA = "TRIP A\n ======\nDeniz Karakay \nAugust van de Ven \nScott Moses \nIsaac Ong \nOana Roșca \nKaisar Arkhan (Yuki) \nDaniel Hsing \nAnshuman Agarwal \nCristian Garcia \nTymon Radzik \nDhruv Shrivastava Sawan Kumar \nJacqueline Bronger \nSoham Sen \nFilip Grzywok \nJustin Du";
-var TripB = "TRIP B\n ======\nEvgeny Shulgin \nAlexandru Bratosin\n Sergey Popov \nIlya Bizyaev \nSampriti Panda \nTommy Ip \n Utkarsh Dixit \nDhanat Satta-awalo \nCollin Grimm \nMira Yang \nJoshua Pan \nShriank Kanaparti \nShardul Chiplunkar \nMatthew Marting \nRaefaldhi Amartya Junior \nVanisha Kesswani \nMichal Hanus \nSudhanshu Agarwal \n"
+var TripB = "TRIP B\n ======\nEvgeny Shulgin \nAlexandru Bratosin\nSergey Popov \nIlya Bizyaev \nSampriti Panda \nTommy Ip \nUtkarsh Dixit \nDhanat Satta-awalo \nCollin Grimm \nMira Yang \nJoshua Pan \nShriank Kanaparti \nShardul Chiplunkar \nMatthew Marting \nRaefaldhi Amartya Junior \nVanisha Kesswani \nMichal Hanus \nSudhanshu Agarwal \n"
 
 var words = [
-	"Trip A", "Trip B", "K", "lol", "Anime", "...", "dude",
-	"@CollinGrimm", "fuck", "stupid", "it works", "who are you"];
-var response = [
-	TripA, TripB, "Kek", "No LOLs", "Is that a thing?", "Dude, enough with the ... thingy xD",
-	"Hey dudie", "Grimm is my god", "The F word is offensive", "You are stupid", "Damn right it works!",
-	"I'm your boss!"];
+	"Trip A", "Trip B", "hello"];
+var response = [TripA, TripB, "Hi there"];
 
 telegram.on("text", (message) => {
 	for (var i = 0; i < words.length; i++) {
 		var res = response[i];
 		if (message.text.toLowerCase().includes(words[i].toLowerCase())) {
-			console.log(message.chat.id);
-			return telegram.sendMessage(message.chat.id, res);
+			return telegram.sendMessage(message.chat.id, res, {parse_mode: "Markdown"});
 		}
 		if (message.text.toLowerCase() == words[i].toLowerCase()) {
 			return telegram.sendMessage(message.chat.id, res);
