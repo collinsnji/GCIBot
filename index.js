efc4eb91a24f4e39cad0f62608f41bf834b7a6de
collinsnji <collins.geek@gmail.com>
2017-06-05 10:16:51 +0100

    Change command system. Using inline commands via bot name

--- index.js
+++ index.js
@@ -6,21 +6,36 @@ var config = JSON.parse(fs.readFileSync("config.json")),
 
 var telegram = new TelegramBotAPI(API_KEY, { polling: true });
 
-var TripA = "TRIP A\n ======\nDeniz Karakay \nAugust van de Ven \nScott Moses \nIsaac Ong \nOana Roșca \nKaisar Arkhan (Yuki) \nDaniel Hsing \nAnshuman Agarwal \nCristian Garcia \nTymon Radzik \nDhruv Shrivastava Sawan Kumar \nJacqueline Bronger \nSoham Sen \nFilip Grzywok \nJustin Du";
-var TripB = "TRIP B\n ======\nEvgeny Shulgin \nAlexandru Bratosin\nSergey Popov \nIlya Bizyaev \nSampriti Panda \nTommy Ip \nUtkarsh Dixit \nDhanat Satta-awalo \nCollin Grimm \nMira Yang \nJoshua Pan \nShriank Kanaparti \nShardul Chiplunkar \nMatthew Marting \nRaefaldhi Amartya Junior \nVanisha Kesswani \nMichal Hanus \nSudhanshu Agarwal \n"
+var TripA = "TRIP A\n ======\nDeniz Karakay\nAugust van de Ven\nScott Moses\nIsaac Ong\nOana Roșca\n\
+			Kaisar Arkhan (Yuki)\nDaniel Hsing\nAnshuman Agarwal\nCristian Garcia \nTymon Radzik \n\
+			Dhruv Shrivastava Sawan Kumar\nJacqueline Bronger \nSoham Sen \nFilip Grzywok \nJustin Du",
 
-var words = [
-	"Trip A", "Trip B", "hello"];
-var response = [TripA, TripB, "Hi there"];
+	TripB = "TRIP B\n ======\nEvgeny Shulgin\nAlexandru Bratosin\nSergey Popov\nIlya Bizyaev\nSampriti Panda\n\
+			Tommy Ip\nUtkarsh Dixit\nDhanat Satta-awalo \nCollin Grimm\nMira Yang \nJoshua Pan\nShriank Kanaparti\n\
+			Shardul Chiplunkar\nMatthew Marting\nRaefaldhi Amartya Junior\nVanisha Kesswani\nMichal Hanus\n\
+			Sudhanshu Agarwal\n",
+
+	TripPics = "Google Code-In Photo Album\n\
+				---------------------------\n\
+				https://goo.gl/photos/z6igidSJYce4Mv3c7",
+
+	HelpMsg = "Usage: @CodeInBot <command>\n\
+			  Where <command> is one of:\n\
+			      Trip A, Trip B, Trip Pictures\n"
+
+var words = ["Trip A", "Trip B", "Help", "Trip Pictures"];
+var response = [TripA, TripB, HelpMsg, TripPics];
 
 telegram.on("text", (message) => {
-	for (var i = 0; i < words.length; i++) {
-		var res = response[i];
-		if (message.text.toLowerCase().includes(words[i].toLowerCase())) {
-			return telegram.sendMessage(message.chat.id, res, {parse_mode: "Markdown"});
-		}
-		if (message.text.toLowerCase() == words[i].toLowerCase()) {
-			return telegram.sendMessage(message.chat.id, res);
+	if (message.text.substr(0, 10) == "@CodeInBot") {
+		for (var i = 0; i < words.length; i++) {
+			var res = response[i];
+			if (message.text.toLowerCase().includes(words[i].toLowerCase())) {
+				return telegram.sendMessage(message.chat.id, res, { parse_mode: "markdown" });
+			}
+			if (message.text.toLowerCase() == words[i].toLowerCase()) {
+				return telegram.sendMessage(message.chat.id, res);
+			}
 		}
 	}
 });
