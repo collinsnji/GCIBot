0f88d11a62e92e7dc75d30fc4514cf46e7db4167
Daniel Hsing <hsing.daniel@gmail.com>
2017-06-06 01:35:04 +0800

    manage multiple webhooks
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const TelegramBotAPI = require('node-telegram-bot-api');
 const fs = require('fs');
 require('dotenv').config();
-
-const { API_KEY } = process.env
-const telegram = new TelegramBotAPI(API_KEY, { polling: true });
-
 const commandOptions = require('./commandOptions')
-
 const failedMessage = `Command not found. Enter command \`/trip help\` to see full list of options`
 
-telegram.on("text", ({ text, chat: { id } }) => {
-	if (text.startsWith("/trip")) {
-		const output = commandOptions[text.split("/trip")[1].trim().toLowerCase()] || failedMessage;
-		return telegram.sendMessage(id, output, {
-			parse_mode: "Markdown"
+const { API_KEY } = process.env
+const telegram = new TelegramBotAPI(API_KEY);
+telegram.openWebHook()
+	.then(telegram.startPolling({
+		restart: true,
+	}))
+	.then(() => {
+		telegram.on("text", ({ text, chat: { id } }) => {
+			if (text.startsWith("/trip")) {
+				const output = commandOptions[text.split("/trip")[1].trim().toLowerCase()] || failedMessage;
+				return telegram.sendMessage(id, output, {
+					parse_mode: "Markdown"
+				});
+			}
 		});
-	}
-});
+	})
+	.catch((err) => {
+		console.log(err);
+	})
