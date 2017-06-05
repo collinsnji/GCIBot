96051feefb7982c4cc40d338a98dd49e46baefde
Daniel Hsing <hsing.daniel@gmail.com>
2017-06-06 01:11:22 +0800

    add md message formatting, patch options formatting
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- index.js
+++ index.js
@@ -5,26 +5,15 @@ require('dotenv').config();
 const { API_KEY } = process.env
 const telegram = new TelegramBotAPI(API_KEY, { polling: true });
 
-const commandOptions = {
-	a: "TRIP A\n ======\nDeniz Karakay\nAugust van de Ven\nScott Moses\nIsaac Ong\nOana Roșca\n\
-			Kaisar Arkhan (Yuki)\nDaniel Hsing\nAnshuman Agarwal\nCristian Garcia \nTymon Radzik \n\
-			Dhruv Shrivastava Sawan Kumar\nJacqueline Bronger \nSoham Sen \nFilip Grzywok \nJustin Du",
-	b:  "TRIP B\n ======\nEvgeny Shulgin\nAlexandru Bratosin\nSergey Popov\nIlya Bizyaev\nSampriti Panda\n\
-			Tommy Ip\nUtkarsh Dixit\nDhanat Satta-awalo \nCollin Grimm\nMira Yang \nJoshua Pan\nShriank Kanaparti\n\
-			Shardul Chiplunkar\nMatthew Marting\nRaefaldhi Amartya Junior\nVanisha Kesswani\nMichal Hanus\n\
-			Sudhanshu Agarwal\n",
-	pictures:  "Google Code-In Photo Album\n\
-				---------------------------\n\
-				https://goo.gl/photos/z6igidSJYce4Mv3c7",
-	dates: "Trip A: Mon Jun 05 - Thu Jun 08\nTrip B: Mon Jun 26 - Thu Jun 29",
-	help: "Usage: /trip <command>\nWhere <command> is one of:\nA, B, Pictures\n(Commands are case insensitive)"
-};
+const commandOptions = require('./commandOptions')
 
 const failedMessage = `Command not found. Enter command \`/trip help\` to see full list of options`
 
 telegram.on("text", ({ text, chat: { id } }) => {
 	if (text.startsWith("/trip")) {
 		const output = commandOptions[text.split("/trip")[1].trim().toLowerCase()] || failedMessage;
-		return telegram.sendMessage(id, output);
+		return telegram.sendMessage(id, output, {
+			parse_mode: "Markdown"
+		});
 	}
 });
