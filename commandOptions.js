654701388bcd48020cd5d5380b51aaa956460663
Daniel Hsing <hsing.daniel@gmail.com>
2017-06-06 01:13:30 +0800

    patch tabbed command options
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- commandOptions.js
+++ commandOptions.js
@@ -1,46 +1,47 @@
 module.exports = {
 	a: `TRIP A
-	Deniz Karakay
-	August van de Ven
-	Scott Moses
-	Isaac Ong
-	Oana Roșca
-	Kaisar Arkhan (Yuki)
-	Daniel Hsing
-	Anshuman Agarwal
-	Cristian Garcia
-	Tymon Radzik
-	Dhruv Shrivastava Sawan Kumar
-	Jacqueline Bronger
-	Soham Sen
-	Filip Grzywok
-	Justin Du`,
+======
+Deniz Karakay
+August van de Ven
+Scott Moses
+Isaac Ong
+Oana Roșca
+Kaisar Arkhan (Yuki)
+Daniel Hsing
+Anshuman Agarwal
+Cristian Garcia
+Tymon Radzik
+Dhruv Shrivastava Sawan Kumar
+Jacqueline Bronger
+Soham Sen
+Filip Grzywok
+Justin Du`,
 	b:  `TRIP B
-    ======
-    Evgeny Shulgin
-    Alexandru Bratosin
-    Sergey Popov
-    Ilya Bizyaev
-    Sampriti Panda
-    Tommy Ip
-    Utkarsh Dixit
-    Dhanat Satta-awalo
-    Collin Grimm
-    Mira Yang
-    Joshua Pan
-    Shriank Kanaparti
-    Shardul Chiplunkar
-    Matthew Marting
-    Raefaldhi Amartya Junior
-    Vanisha Kesswani
-    Michal Hanus
-    Sudhanshu Agarwal`,
+======
+Evgeny Shulgin
+Alexandru Bratosin
+Sergey Popov
+Ilya Bizyaev
+Sampriti Panda
+Tommy Ip
+Utkarsh Dixit
+Dhanat Satta-awalo
+Collin Grimm
+Mira Yang
+Joshua Pan
+Shriank Kanaparti
+Shardul Chiplunkar
+Matthew Marting
+Raefaldhi Amartya Junior
+Vanisha Kesswani
+Michal Hanus
+Sudhanshu Agarwal`,
 	pictures:  `Google Code-In Photo Album
-    ---------------------------
-    https://goo.gl/photos/z6igidSJYce4Mv3c7`,
+---------------------------
+https://goo.gl/photos/z6igidSJYce4Mv3c7`,
 	dates: "Trip A: Mon Jun 05 - Thu Jun 08\nTrip B: Mon Jun 26 - Thu Jun 29",
 	help: `Usage: /trip <command>
-    Where <command> is one of:
-    A, B, Pictures
-    (Commands are case insensitive)`
+Where <command> is one of:
+A, B, Pictures
+(Commands are case insensitive)`
 };
