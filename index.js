34b14a2458aa7f85ae56b3ce5acd94c50e9b2566
Daniel Hsing <hsing.daniel@gmail.com>
2017-06-06 00:41:10 +0800

    Add default config file, condense main code, add message on failure
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- index.js
+++ index.js
@@ -1,40 +1,30 @@
 const TelegramBotAPI = require('node-telegram-bot-api');
 const fs = require('fs');
 
-var config = JSON.parse(fs.readFileSync("config.json")),
+const config = JSON.parse(fs.readFileSync("config.json")),
 	API_KEY = config.API_KEY;
+const telegram = new TelegramBotAPI(API_KEY, { polling: true });
 
-var telegram = new TelegramBotAPI(API_KEY, { polling: true });
-
-var TripA = "TRIP A\n ======\nDeniz Karakay\nAugust van de Ven\nScott Moses\nIsaac Ong\nOana Roșca\n\
+const commandOptions = {
+	a: "TRIP A\n ======\nDeniz Karakay\nAugust van de Ven\nScott Moses\nIsaac Ong\nOana Roșca\n\
 			Kaisar Arkhan (Yuki)\nDaniel Hsing\nAnshuman Agarwal\nCristian Garcia \nTymon Radzik \n\
 			Dhruv Shrivastava Sawan Kumar\nJacqueline Bronger \nSoham Sen \nFilip Grzywok \nJustin Du",
-
-	TripB = "TRIP B\n ======\nEvgeny Shulgin\nAlexandru Bratosin\nSergey Popov\nIlya Bizyaev\nSampriti Panda\n\
+	b:  "TRIP B\n ======\nEvgeny Shulgin\nAlexandru Bratosin\nSergey Popov\nIlya Bizyaev\nSampriti Panda\n\
 			Tommy Ip\nUtkarsh Dixit\nDhanat Satta-awalo \nCollin Grimm\nMira Yang \nJoshua Pan\nShriank Kanaparti\n\
 			Shardul Chiplunkar\nMatthew Marting\nRaefaldhi Amartya Junior\nVanisha Kesswani\nMichal Hanus\n\
 			Sudhanshu Agarwal\n",
-
-	TripPics = "Google Code-In Photo Album\n\
+	pictures:  "Google Code-In Photo Album\n\
 				---------------------------\n\
 				https://goo.gl/photos/z6igidSJYce4Mv3c7",
+	dates: "Trip A: Mon Jun 05 - Thu Jun 08\nTrip B: Mon Jun 26 - Thu Jun 29",
+	help: "Usage: /trip <command>\nWhere <command> is one of:\nA, B, Pictures\n(Commands are case insensitive)"
+};
 
-	TripDates = "Trip A: Mon Jun 05 - Thu Jun 08\nTrip B: Mon Jun 26 - Thu Jun 29",
-	HelpMsg = "Usage: /trip <command>\nWhere <command> is one of:\nA, B, Pictures\n(Commands are case insensitive)";
+const failedMessage = `Command not found. Enter command \`/trip help\` to see full list of options`
 
-var words = ["A", "B", "Help", "Pictures", "Dates"];
-var response = [TripA, TripB, HelpMsg, TripPics, TripDates];
-
-telegram.on("text", (message) => {
-	var cmdExtract = message.text.toLowerCase().substr(0, 5);
-	var msgText = (message.text.toLowerCase().substr(6, message.text.length));
-
-	if (cmdExtract == "/trip") {
-		for (var i = 0; i < words.length; i++) {
-			var res = response[i];
-			if (msgText.toLowerCase().trim() == words[i].toLowerCase()) {
-				return telegram.sendMessage(message.chat.id, res);
-			}
-		}
+telegram.on("text", ({ text, chat: { id } }) => {
+	if (text.startsWith("/trip")) {
+		const output = commandOptions[text.split("/trip")[1].trim().toLowerCase()] || failedMessage;
+		return telegram.sendMessage(id, output);
 	}
 });
